703fa046fbe5e88b41dfc2d66d861e57ea0b715a
Luiz Otávio <luizzotavionazar@outlook.com>
2024-10-16 21:29:03 -0300

    Questão 2 - Finalizada

---
+++ Atv2.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Atv2 {
+    public static void main(String[] args) {
+        Scanner in= new Scanner(System.in);
+        ArrayList<Integer> numeros= new ArrayList<>();
+        boolean control= true;
+        int soma=0, seq= 0;
+
+        System.out.println("");
+        System.out.println("Informe os números que deseja somar, digite 'sair' para encerrar:");
+        System.out.println("");
+
+        while (control) {
+            System.out.print((seq+1)+"º Valor: ");
+
+            try {
+                numeros.add(in.nextInt());
+                soma= soma+numeros.get(0);
+                numeros.remove(0);
+                seq++;
+            } catch (Exception e) {
+                control= false;
+            }
+            
+        }
+
+        System.out.println("");
+        System.out.println("Soma final: "+soma);
+        System.out.println("");
+
+    }
+}
