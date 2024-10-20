d615fe6312079b5d29716cf9c7336b0863f15394
Luiz Otavio <luizzotavionazar@outlook.com>
2024-10-20 11:43:07 -0300

    Questão 4 - Finalizada

---
+++ Revisão Java/Atv4.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Atv4 {
+    public static void main(String[] args) {
+        Scanner in= new Scanner(System.in);
+        String frase= null;
+
+        System.out.println("");
+        System.out.println("Escreva uma frase, será contada quantas palavras ela possui!");
+
+        System.out.println("");
+        System.out.print("Frase: ");
+        frase= in.nextLine();
+
+        StringTokenizer separador= new StringTokenizer(frase, " ,!@?:;./|=+-()*&¨%$#'><[]^~°ºª§");
+
+        System.out.println("");
+        System.out.println("Quantidade de palavras: "+separador.countTokens());
+        System.out.println("");
+
+        in.close();
+    }
+}
